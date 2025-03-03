047d0a35e94990efa148a4ea3cbf3da2fac55875
Matis250 <matishebert@outlook.com>
2025-03-03 01:06:46 +0000

    add my content

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Matis Hébert",
+  title: "Adjoint Administratif",
+  email: "matishebert",
+  gitHub: "matis250",
+  instagram: "matxs_250",
+  linkedIn: "Matis Hébert",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,12 +31,12 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Esprit entrepreunarial",
+  "Service à la clientèle",
+  "Finance et assurace",
+  "Travail d'équipe",
+  "Connaissances technologiques",
+  "Athlète",
 ];
 
 /**
